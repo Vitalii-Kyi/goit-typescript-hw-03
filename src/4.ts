6eb714c618a033323f2cffea14cc9ac273b3abf5
Vitalii Kyi <kyi.profy@gmail.com>
2023-12-12 20:44:06 +0200

    finished

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,43 @@
+class Key {
+  private signature: number = Math.random();
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+  comeIn(person: Person) {
+    this.tenants.push(person);
+  }
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
